8f384da908400d20030949fe11197edfdc35bed0
Lizandra Malta <lizlrmalta@gmail.com>
2026-06-17 15:48:27 -0300

    first commit

---
+++ Bentoshi/Bentoshi/BentoshiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentoshiApp.swift
+//  Bentoshi
+//
+//  Created by Lizandra Malta on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BentoshiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
